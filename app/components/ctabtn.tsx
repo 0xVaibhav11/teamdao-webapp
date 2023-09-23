d48dd59e15875edb2663c336f4946cfafc4c65f0
0xVaibhav11 <vanoobcoder@gmail.com>
2023-09-23 17:10:38 +0530

    ctabtn changed

--- app/components/ctabtn.tsx
+++ app/components/ctabtn.tsx
@@ -4,7 +4,7 @@ import styles from "../page.module.scss";
 export default function CtaBtn(props: any) {
   return (
     <div className={styles.cta}>
-      <a href="#team-up" className={styles.ctaBtn}>
+      <a href="https://t.me/TEAMDAO" target="_blank" className={styles.ctaBtn}>
         <div className={styles.bg} />
         <div className={styles.shine} />
         <span>JOIN NOW</span>
