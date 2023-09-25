6b6d63f108a0b19173c1fc0820ee42e71019312d
0xVaibhav11 <vanoobcoder@gmail.com>
2023-09-25 12:42:31 +0530

    changed cta link

--- app/components/ctabtn.tsx
+++ app/components/ctabtn.tsx
@@ -4,7 +4,11 @@ import styles from "../page.module.scss";
 export default function CtaBtn(props: any) {
   return (
     <div className={styles.cta}>
-      <a href="https://t.me/TEAMDAO" target="_blank" className={styles.ctaBtn}>
+      <a
+        href="https://t.me/BattleForSupremacyTV"
+        target="_blank"
+        className={styles.ctaBtn}
+      >
         <div className={styles.bg} />
         <div className={styles.shine} />
         <span>JOIN NOW</span>
